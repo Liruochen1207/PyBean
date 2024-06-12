5c206c5b10c4a5b89b575c3522c55121474d6024
ArcherLee_rc <liruochen01@outlook.com>
2024-06-12 17:47:22 +0800

    0.0.21

---
+++ PyBean/by.py
@@ -0,0 +1,8 @@
+class Default:
+    pass
+
+
+class By:
+    id = 0
+    name = 1
+    clazz = 2
--- test/testIoc.py
+++ test/testIoc.py
@@ -1,15 +1,21 @@
 from PyBean import bean
+from PyBean.by import By
 from PyBean.ioc import Application
 
 
 if __name__ == '__main__':
     ioc = Application('resource/applicationContext.xml')
     print(ioc.getBeanList())
-    bookDao: bean = ioc.getBean('bookDao')
-    bookImp: bean = ioc.getBean('bookImp')
+    bookDao: bean = ioc.getBean(by=By.id, arg='bookDao', requiredType='dao.bookdao.Bookdao')
+    bookImp: bean = ioc.getBean('bookImp', requiredType='imp.bookImp.BookDaoImp')
+    # print(ioc.getBean(requiredType='imp.bookImp.BookDaoImp'))
 
-    getDao: bean = ioc.matchBean('dao', bookImp)
 
+    className = bookDao.attribute("class")
+    getDao: bean = ioc.getBean(requiredType=className)
+    print(getDao.instance == bookImp.instance)
+    print(type(getDao.instance) == type(bookImp.instance))
+
+    bookDao.instance.show()
     bookImp.instance.show()
     getDao.instance.show()
-    # bookDao.instance.show()
